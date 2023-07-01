7d5a810f1580459e01babaa26007c728d26f8f7e
C. Serrano <serrano.cjm@gmail.com>
2023-07-01 10:05:50 -0400

    01 - Intro to Compose

--- app/src/main/java/com/example/helloworldjetpackcompose/MainActivity.kt
+++ app/src/main/java/com/example/helloworldjetpackcompose/MainActivity.kt
@@ -4,29 +4,43 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.helloworldjetpackcompose.ui.theme.HelloWorldJetpackComposeTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            HelloWorldJetpackComposeTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
-            }
+            HelloApp()
         }
     }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun HelloApp() {
+    HelloWorldJetpackComposeTheme {
+        // A surface container using the 'background' color from the theme
+        Surface(
+            modifier = Modifier.fillMaxSize(),
+            color = MaterialTheme.colorScheme.background
+        ) {
+            Greeting("Totona")
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun ExampleModifier() {
+    Text(text = "Text view normal", modifier = Modifier.padding(16.dp))
 }
 
 @Composable
@@ -41,6 +55,14 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Composable
 fun GreetingPreview() {
     HelloWorldJetpackComposeTheme {
-        Greeting("Android")
+        Greeting("Captain Arepa")
+    }
+}
+
+@Preview(showBackground = true, name = "Another Preview")
+@Composable
+fun AnotherGreetingPreview() {
+    HelloWorldJetpackComposeTheme {
+        Greeting("Captain Arepa")
     }
 }
